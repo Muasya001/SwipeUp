d9f654289492bcdf5c7f24177da07c98b603b799
Muasya <muasyapaul001@gmail.com>
2023-05-23 00:38:01 +0300

    Initial commit

--- app/src/main/java/com/example/swipeup/MainActivity.kt
+++ app/src/main/java/com/example/swipeup/MainActivity.kt
@@ -10,10 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.swipeup.ui.theme.SwipeUpTheme
 import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.SwipeRefreshIndicator
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 
 class MainActivity : ComponentActivity() {
@@ -24,10 +26,22 @@ class MainActivity : ComponentActivity() {
                 val viewModel = viewModel<MainViewModel>()
                 val isLoading by viewModel.isLoading.collectAsState()
                 val swipeRefreshState= rememberSwipeRefreshState(isRefreshing=isLoading)
+                SwipeRefresh(state = swipeRefreshState, onRefresh = viewModel::loadStuff,
+                indicator = {
+                    state, refreshTrigger ->
+                    SwipeRefreshIndicator(state = state, refreshTriggerDistance = refreshTrigger,
+                    backgroundColor = Color.Blue,
+                    contentColor = Color.DarkGray)
+                }
+                ) {
+                    
+                }
                 SwipeRefresh(state = swipeRefreshState, onRefresh = viewModel:: loadStuff) {
-                    LazyColumn(modifier = Modifier.fillMaxSize()){
+                    LazyColumn(modifier = Modifier.fillMaxSize())
+                    {
                         items(count = 100){
-                            Text(text = "test me", modifier = Modifier.fillMaxSize()
+                            Text(text = "test me", modifier = Modifier
+                                .fillMaxSize()
                                 .padding(32.dp))
                         }
                     }
